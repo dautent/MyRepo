d67e0d0deccafc3aef0101144eb989b99009750a
dautent <credityciva@gmail.com>
2025-03-17 11:49:22 +0800

    update

--- hot_rank.py
+++ hot_rank.py
@@ -5,10 +5,11 @@ from datetime import datetime
 import subprocess  # 用于 Git 操作
 
 # 数据存储路径
-CSV_FILE = os.path.join(os.getenv("GITHUB_WORKSPACE", ""), "人气榜.csv")
+CSV_FILE = os.path.join(os.getenv("GITHUB_WORKSPACE", ""), "hot_rank.csv.gz")
+LOG_FILE = os.path.join(os.getenv("GITHUB_WORKSPACE", ""), "hot_rank_log.txt")
 
 def fetch_hot_rank():
-    """获取当天 A 股人气榜数据"""
+    """获取当天数据"""
     try:
         df = ak.stock_hot_rank_em()
         if df.empty:
@@ -24,7 +25,7 @@ def load_existing_data():
     """加载本地存储的 CSV 数据"""
     if os.path.exists(CSV_FILE):
         try:
-            return pd.read_csv(CSV_FILE)
+            return pd.read_csv(CSV_FILE, compression="gzip")
         except Exception as e:
             print(f"读取历史数据失败: {e}")
             return None
@@ -41,7 +42,7 @@ def save_to_csv(new_data):
         combined_data = new_data
 
     try:
-        combined_data.to_csv(CSV_FILE, index=False)
+        combined_data.to_csv(CSV_FILE, index=False, compression="gzip")
         print(f"数据已保存至 {CSV_FILE}")
     except Exception as e:
         print(f"保存失败: {e}")
@@ -52,7 +53,7 @@ def save_to_csv(new_data):
         subprocess.run(["git", "config", "--global", "user.name", "github-actions"], check=True)
         subprocess.run(["git", "config", "--global", "user.email", "github-actions@github.com"], check=True)
         subprocess.run(["git", "add", CSV_FILE], check=True)
-        subprocess.run(["git", "commit", "-m", f"更新人气榜数据 {datetime.now().strftime('%Y-%m-%d')}"], check=True)
+        subprocess.run(["git", "commit", "-m", f"更新数据 {datetime.now().strftime('%Y-%m-%d')}"], check=True)
         subprocess.run(["git", "push"], check=True)
         print("数据推送成功 ✅")
     except Exception as e:
@@ -63,6 +64,11 @@ def main():
     new_data = fetch_hot_rank()
     if new_data is not None:
         save_to_csv(new_data)
+
+    #保存日志
+    today = new_data["date"].iloc[-1]
+    with open(LOG_FILE, "a", encoding="utf-8") as f:
+        f.write(f"{today}\n")
 
 if __name__ == "__main__":
     main()
